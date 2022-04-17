2be5dc778994ffa7fce0602fe3a4248329ef7bde
EC2 Default User <[aria.bravemen@gmail.com]>
2022-04-17 09:06:51 +0000

    レイアウト調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,5 @@
 class BooksController < ApplicationController
 
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -13,10 +9,12 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
     @user = @book.user
   end
 
